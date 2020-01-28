7f723293bb3d65358063f88a8b8fea8f741dcbbf
Your Name <you@example.com>
2020-01-27 23:30:33 -0800

    min idiom length is now an input

--- get_idiom_comments.py
+++ get_idiom_comments.py
@@ -1,13 +1,13 @@
 import sys
 import json
 
-def load_idioms(filename):
+def load_idioms(filename, min_idiom_length):
     idioms = set()
     with open(filename,'r') as f:
         f.readline()
         for l in f:
             l = l.strip().split('\t')
-            if len(l[0].split()) > 5:
+            if len(l[0].split()) > min_idiom_length:
                 idioms.add(l[0])
     return idioms
 
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     if len(sys.argv) != 5:
         print('Usage: get_comments_with_idiom.py <idiom_input_path> <comment_input_path> <output_path> <min_length>')
         exit()
-    idioms = load_idioms(sys.argv[1])
     min_idiom_length = int(sys.argv[4])
+    idioms = load_idioms(sys.argv[1],min_idiom_length)
     print("{} idioms with length > {} found".format(len(idioms), min_idiom_length))
     with open(sys.argv[2],'r') as f:
         with open(sys.argv[3], 'w') as w:
