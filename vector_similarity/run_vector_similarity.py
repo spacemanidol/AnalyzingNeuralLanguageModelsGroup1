98339fd385ce5cdf456cfc34f39d61756caa19ef
Paige Finkelstein <paigelfink@gmail.com>
2020-03-06 11:27:37 -0500

    clean up word comp vector similarity code

--- vector_similarity/run_vector_similarity.py
+++ vector_similarity/run_vector_similarity.py
@@ -10,6 +10,8 @@ from sklearn.decomposition import PCA
 import numpy as np
 import argparse
 import time
+import random
+import logging
 
 words, paraphrase_sent_pairs = 'words', 'para_pairs'
 
@@ -23,25 +25,24 @@ def sentence_paraphrase_comparisons(input_args):
     dataset = SentenceParaphraseInspectionDataset(input_args.input, input_args.embedding_model, 
                                                 input_args.embedding_batch_size, input_args.run_name)
     embeddings = get_embeddings(dataset, input_args.embedding_cache, flattened=True)
-    embedding_outputs, encoded_inputs, _indices = embeddings
+    embedding_outputs, encoded_inputs, _indices, _pools = embeddings
     sentence_embeddings = get_sentence_embeddings(embeddings, dataset)
 
-    paraphrase_cosine_metrics = calculate_sentence_paraphrase_cosine_metrics(dataset, embedding_outputs, 
+    paraphrase_cosine_metrics = calculate_sent_cosine_metrics(dataset, embedding_outputs, 
                                                                             encoded_inputs, sentence_embeddings)
     # print(paraphrase_cosine_metrics)
     print(summarize_sentence_similarity_comp(paraphrase_cosine_metrics))
-
 
 def word_usage_comparisons(input_args):
     dataset = WordInspectionDataset(input_args.input, input_args.embedding_model, 
                                     input_args.embedding_batch_size, input_args.run_name)
     embeddings = get_embeddings(dataset, input_args.embedding_cache, flattened=False)
-    embedding_outputs, encoded_inputs, _indices = embeddings
+    embedding_outputs, encoded_inputs, _indices, _pools = embeddings
     data = dataset.get_data()
     idiom_sentence_indexes = get_idiom_sentences(data)
 
     word_sim_results = calculate_word_cosine_metrics(dataset, embedding_outputs, encoded_inputs, idiom_sentence_indexes)
-    print(word_sim_results)
+    # print(word_sim_results)
     print(summarize_word_similarity_comp(word_sim_results))
 
     PCA_comparisions(dataset, embedding_outputs, encoded_inputs, idiom_sentence_indexes)
@@ -56,18 +57,19 @@ def get_embeddings(data, embedding_cache, flattened):
     return data.load_saved_embeddings(embedding_cache)
 
 def get_sentence_embeddings(embeddings, data):
-    embedding_outputs, encoded_inputs, indices = embeddings
+    embedding_outputs, encoded_inputs, indices, _pools = embeddings
     return data.aggregate_sentence_embeddings(embedding_outputs, encoded_inputs, indices)
 
-def calculate_sentence_paraphrase_cosine_metrics(dataset, embedding_outputs, encoded_inputs, sentence_embeddings):
+def calculate_sent_cosine_metrics(dataset, embedding_outputs, encoded_inputs, sentence_embeddings):
     data = dataset.get_data()
     paraphrase_cosine_metrics = [calculate_paraphrase_pair_similarity(i, pair_sents, dataset, encoded_inputs, sentence_embeddings) 
-                            for i, pair_sents in enumerate(data)]
+                                for i, pair_sents in enumerate(data)]
     return paraphrase_cosine_metrics
 
 def calculate_word_cosine_metrics(dataset, embedding_outputs, encoded_inputs, idiom_sentence_indexes):
-    word_cosine_metrics = [calculate_word_similarity_metrics(idiom_sent_idx, dataset, embedding_outputs, encoded_inputs) 
-                            for idiom_sent_idx in idiom_sentence_indexes]
+    word_cosine_metrics = [calculate_word_similarity_metrics(idiom_sent_idx_group, dataset, 
+                                                             embedding_outputs, encoded_inputs) 
+                            for idiom_sent_idx_group in idiom_sentence_indexes]
     return word_cosine_metrics
 
 def calculate_paraphrase_pair_similarity(index, classifier_out, dataset, encoded_inputs, sentence_embeddings):
@@ -81,36 +83,50 @@ def calculate_paraphrase_pair_similarity(index, classifier_out, dataset, encoded
         'cosine_similarity': cosine_sim
     }    
 
-def calculate_word_similarity_metrics(idiom_sent_index, dataset, embedding_outputs, encoded_inputs):
+def calculate_word_similarity_metrics(idiom_sent_index_group, dataset, embedding_outputs, encoded_inputs, random_indexes=None):
     data = dataset.get_data()
-    idiom_ex = data[idiom_sent_index]
-    idiom_word_embedding = get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, idiom_sent_index)
+    idiom_exs = [data[idiom_sent_index] for idiom_sent_index in idiom_sent_index_group]
+    idiom_word_embeddings = [get_word_embedding(dataset, data, embedding_outputs, 
+                                               encoded_inputs, idiom_sent_index) 
+                            for idiom_sent_index in idiom_sent_index_group]
     cosine_similarity_metrics = {}
+    
+    pair_id = idiom_exs[0].pair_id
+    idiom_word = idiom_exs[0].word
 
-    literal_usage_sents = [i for i, ex in enumerate(data) if ex.pair_id == idiom_ex.pair_id and 
-                                                            ex.word == idiom_ex.word and not 
-                                                            ex.sentence_id == idiom_ex.sentence_id ]
-    paraphrase_sents = [i for i, ex in enumerate(data) if ex.pair_id == idiom_ex.pair_id 
-                                                            and not ex.word == idiom_ex.word]
+    literal_usage_sents = [i for i, ex in enumerate(data) if ex.pair_id == pair_id and 
+                                                            ex.word == idiom_word and not 
+                                                            ex.figurative ]
+    paraphrase_sents = [i for i, ex in enumerate(data) if ex.pair_id == pair_id 
+                                                            and not ex.word == idiom_word]
 
-    literal_usage_embeddings = [get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, lit_idx) for lit_idx in literal_usage_sents]
-    paraphrase_embeddings = [get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, para_idx) for para_idx in paraphrase_sents]
+    literal_usage_embeddings = [get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, lit_idx) 
+                                for lit_idx in literal_usage_sents]
+    paraphrase_embeddings = [get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, para_idx) 
+                             for para_idx in paraphrase_sents]
 
-    cosine_similarity_metrics['fig_to_literal'] = calculate_cosine_similarity_average([idiom_word_embedding], literal_usage_embeddings)
+    cosine_similarity_metrics['fig_to_literal'] = calculate_cosine_similarity_average(idiom_word_embeddings, literal_usage_embeddings)
     cosine_similarity_metrics['literal_to_literal'] = calculate_cosine_similarity_average(literal_usage_embeddings)
-    cosine_similarity_metrics['fig_to_paraphrase'] = calculate_cosine_similarity_average([idiom_word_embedding], paraphrase_embeddings)
+    cosine_similarity_metrics['fig_to_paraphrase'] = calculate_cosine_similarity_average(idiom_word_embeddings, paraphrase_embeddings)
     cosine_similarity_metrics['literal_to_paraphrase'] = calculate_cosine_similarity_average(literal_usage_embeddings, paraphrase_embeddings)
     
+    if random_indexes:
+        random_embeddings = [get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, random_idx) 
+                             for random_idx in random_indexes]
+        cosine_similarity_metrics['fig_to_random'] = calculate_cosine_similarity_average(idiom_word_embeddings, random_embeddings)
+
     return {
-        'sentence_id': idiom_ex.sentence_id,
-        'sentence': dataset.decode(encoded_inputs[idiom_sent_index].tolist()),
-        'word': idiom_ex.word,
+        'pair_id': pair_id,
+        'sentences': [dataset.decode(encoded_inputs[idiom_sent_index].tolist()) for idiom_sent_index in idiom_sent_index_group],
+        'word': idiom_word,
         'paraphrase_word': data[paraphrase_sents[0]].word,
         'cosine_similarities': cosine_similarity_metrics,
     }
 
 def get_idiom_sentences(dataset):
-    return [i for i, ex in enumerate(dataset) if ex.figurative]
+    idioms = [(i, ex) for i, ex in enumerate(dataset) if ex.figurative]
+    values = set(map(lambda x:x[1].pair_id, idioms))
+    return [[y[0] for y in idioms if y[1].pair_id == x] for x in values]
 
 def get_sentences_for_idiom_sentence(dataset, idiom_sent):
     literal_usage_sents = [i for i, ex in enumerate(dataset) if ex.pair_id == idiom_sent.pair_id and 
@@ -123,6 +139,11 @@ def get_sentences_for_idiom_sentence(dataset, idiom_sent):
 def get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, dataset_index):
     ex = data[dataset_index]
     decoded_tokens = dataset.get_decoded_tokens(encoded_inputs[dataset_index].tolist())
+
+# for sanity checking the dataset embeddings and encoded outputs are aligned:
+#   print(ex.sentence)
+#   print(decoded_tokens)
+
     word_index = decoded_tokens.index(ex.word[0])
     return embedding_outputs[dataset_index][word_index]
 
@@ -134,6 +155,7 @@ def calculate_cosine_similarity_average(embeddings_1, embeddings_2=None):
 
     cosine_similarities = [1 - cosine(embedding_1, embedding_2) for embedding_1, embedding_2 in embedding_pairs]
     return mean(cosine_similarities)
+
 
 # This computes the average difference in cosine similarity between:
 # 1.) literal to literal usages versus figurative to literal usage
@@ -147,6 +169,7 @@ def summarize_word_similarity_comp(results):
         'fig_to_paraphrase_improvement_over_lit_to_paraphrase': mean(fig_to_paraphrase_advantage)
     }
     return summary_stats
+
 
 # This computes the average cosine similarity scores between paraphrase pairs,
 # grouped into 4 categories based on gold label (i.e. true paraphrase or not) and classifier judgment
@@ -175,40 +198,88 @@ def handle_zero_case(category_results):
 
 
 # PCA visualization code
-# TODO: figure out what we actually want to include/disclude here and clean up
-
 def PCA_comparisions(dataset, embedding_outputs, encoded_inputs, idiom_sentence_indexes):
     data = dataset.get_data()
-    for idiom_sent_index in idiom_sentence_indexes:
-        idiom_ex = data[idiom_sent_index]
-        idiom_word_embedding = get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, idiom_sent_index)
-        literal_usage_sents = [i for i, ex in enumerate(data) if ex.pair_id == idiom_ex.pair_id and 
-                                                                ex.word == idiom_ex.word and not 
-                                                                ex.sentence_id == idiom_ex.sentence_id ]
-        paraphrase_sents = [i for i, ex in enumerate(data) if ex.pair_id == idiom_ex.pair_id 
-                                                                and not ex.word == idiom_ex.word]
+    for num, idiom_sent_index_group in enumerate(idiom_sentence_indexes):
+        idiom_exs = [data[idiom_sent_index] for idiom_sent_index in idiom_sent_index_group]
+        idiom_word_embeddings = [get_word_embedding(dataset, data, embedding_outputs, 
+                                               encoded_inputs, idiom_sent_index) 
+                                for idiom_sent_index in idiom_sent_index_group]
+        pair_id = idiom_exs[0].pair_id
+        idiom_word = idiom_exs[0].word[0]
+        literal_usage_sents = [i for i, ex in enumerate(data) if ex.pair_id == pair_id and 
+                                                                ex.word[0] == idiom_word and not ex.figurative ]
+        paraphrase_sents = [i for i, ex in enumerate(data) if ex.pair_id == pair_id and not ex.word[0] == idiom_word]
 
-        literal_usage_embeddings = [get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, lit_idx) for lit_idx in literal_usage_sents]
-        paraphrase_embeddings = [get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, para_idx) for para_idx in paraphrase_sents]
+        literal_usage_embeddings = [get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, lit_idx) 
+                                    for lit_idx in literal_usage_sents]
+        
+        paraphrase_embeddings = [get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, para_idx) 
+                                 for para_idx in paraphrase_sents]
 
-        title = 'PCA for {}: Fig usage {} Paraphrase word: {}'.format(idiom_ex.word[0], " ".join(idiom_ex.sentence), data[paraphrase_sents[0]].word)
+        logger = logging.getLogger()
+        logger.setLevel(logging.CRITICAL)
+
+        literal_labels = len(literal_usage_embeddings) * [1]
+        idiom_labels = len(idiom_word_embeddings) * [0]
+        paraphrase_labels = len(paraphrase_embeddings) * [2]
+        
+        print("\n\n\n\n\nExample " + str(num))
+        print("Figurative phrases:  ")
+        for idiom_ex in idiom_exs:
+            print(" ".join(idiom_ex.sentence))
+        print()
+        print("Idiom usage word:  " + idiom_word)
+        print("Paraphrase word:  " + data[paraphrase_sents[0]].word[0])
+        
+        # First PCA graph: only literal and figurative usages
+        title = 'PCA for: "{}"'.format(idiom_word)
         targets = {
             'labels': ['figurative', 'literal'],
             'values': [0, 1] ,
             'colors': ['turquoise', 'navy'],
         }
-        labels =  np.array(len(literal_usage_embeddings) * [1] + [0])
-        show_PCS(literal_usage_embeddings + [idiom_word_embedding], labels, targets, title)
 
-        title = 'PCA for {}: Fig usage {}'.format(idiom_ex.word[0], " ".join(idiom_ex.sentence))
+        labels =  np.array(literal_labels + idiom_labels)
+        show_PCS(literal_usage_embeddings + idiom_word_embeddings, labels, targets, title)
+
+        # Second PCA graph: literal, figurative, and paraphrases
+        title = 'PCA for: "{}"; Paraphrase word: {}'.format(idiom_word, data[paraphrase_sents[0]].word)
         targets = {
             'labels': ['figurative', 'literal', 'paraphrase'],
             'values': [0, 1, 2] ,
             'colors': ['turquoise', 'navy', 'orangered'],
         }
-        embeddings = literal_usage_embeddings + [idiom_word_embedding] + paraphrase_embeddings
-        labels =  np.array(len(literal_usage_embeddings) * [1] + [0] + len(paraphrase_embeddings) * [2])
+        embeddings = literal_usage_embeddings + idiom_word_embeddings + paraphrase_embeddings
+        labels =  np.array(literal_labels + idiom_labels + paraphrase_labels)
         show_PCS(embeddings, labels, targets, title)
+
+        # Third PCA graph: literal, figurative, paraphrases, and random word
+        random_id = random.choice([999, 899, 799])
+        random_word_sents = [i for i, ex in enumerate(data) if ex.pair_id == random_id]
+        random_word_embeddings = [get_word_embedding(dataset, data, embedding_outputs, encoded_inputs, rnd_idx) 
+                                  for rnd_idx in random_word_sents]
+  
+        title = 'PCA for: "{}"; Paraphrase word: {}; Random word: {}'.format(idiom_word, 
+                                                                        data[paraphrase_sents[0]].word,
+                                                                        data[random_word_sents[0]].word)
+        targets = {
+            'labels': ['figurative', 'literal', 'paraphrase', 'random'],
+            'values': [0, 1, 2, 3] ,
+            'colors': ['turquoise', 'navy', 'orangered', 'gray'],
+        }
+        
+        embeddings = literal_usage_embeddings + idiom_word_embeddings + paraphrase_embeddings + random_word_embeddings
+        labels =  np.array(literal_labels + idiom_labels + paraphrase_labels + len(random_word_embeddings) * [3])
+        show_PCS(embeddings, labels, targets, title)
+        
+        word_cosine_results = calculate_word_similarity_metrics(idiom_sent_index_group, dataset,
+                                                                embedding_outputs, 
+                                                                encoded_inputs, random_word_sents)['cosine_similarities'] 
+        
+        print("Cosine similarity scores")
+        for pair_type, cosine_val in word_cosine_results.items():
+            print(pair_type, ": " + str(cosine_val))
 
 def show_PCS(embeddings, labels, targets, title):
     pca = PCA(2)  
