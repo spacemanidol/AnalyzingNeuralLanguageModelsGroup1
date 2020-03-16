07fde117b6d879b5632207daf9a4a3f718d0acb7
Joshua Tanner <mindful.jt@gmail.com>
2020-03-16 14:15:23 -0700

    delete some unused files

--- Classifier/run.sh
+++
@@ -1,4 +0,0 @@
-echo "Training and Testing Bilstm"
-python model.py --data_dir $1/cola_formated --train --evaluate --model bilstm
-echo "Training and Testing Bert"
-python model.py --data_dir $1/cola_formated --train --evaluate --model bert
